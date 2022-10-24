4c6ccf2f15f2c9e948c05f689e8bf991b24234be
TuudurUtt <116277317+TuudurUtt@users.noreply.github.com>
2022-10-24 19:44:47 +0300

    Commit amazon scraper v 1.0

---
+++ amazon_scraper.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+from googletrans import Translator
+
+
+translator = Translator()
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+    " Chrome/106.0.0.0 Safari/537.36"
+}
+
+
+def scrape_listing_links(searched_item: str, suffix: str,
+                         iteration=1, product_links=None, nr_of_pages=None) -> set[str]:
+    if product_links is None:
+        product_links = []
+    r = requests.get(f"https://www.amazon.{suffix}/s?k={searched_item}&page={iteration}", headers=headers)
+    html = BeautifulSoup(r.content, "lxml")
+    product_links += {f"https://www.amazon.{suffix}" + listing.get("href") for listing in
+                      html.find_all("a", class_="a-link-normal s-underline-text s-underline-link-text"
+                                                " s-link-style a-text-normal")
+                      if not listing.get("href").startswith("/sspa/")}
+    if nr_of_pages is None:
+        try:
+            nr_of_pages = int(html.find("span", class_="s-pagination-item s-pagination-disabled").text)
+        except AttributeError:
+            nr_of_pages = 1
+    if nr_of_pages != iteration:
+        return scrape_listing_links(searched_item, suffix,
+                                    iteration=iteration + 1, product_links=product_links, nr_of_pages=nr_of_pages)
+    else:
+        return product_links
+
+
+def scrape_amazon(searched_item: str, suffix: str) -> list[list[str]]:
+    links = scrape_listing_links(searched_item, suffix)
+    output_list = []
+    for link in links:
+        print(link)
+        r = requests.get(link, headers=headers)
+        html = BeautifulSoup(r.content, "lxml")
+        title = translator.translate(html.find("span", id="productTitle").get_text().strip()).text
+        product_price = html.find("span", class_="a-offscreen").get_text().strip().replace(",", ".")
+        image_link = html.find("div", id="imgTagWrapperId").find("img").get("src")
+        if not product_price[:-1].replace(".", "").isnumeric():
+            product_price = "Varies"
+        output_list.append([title, product_price, image_link, link])
+    return output_list
--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
-    " Chrome/106.0.0.0 Safari/537.36"
-}
-
-
-def scrape_amazon(searched_item: str, suffix: str) -> list[str]:
-    base_url = f"https://www.amazon.{suffix}/s?k={searched_item}"
-    r = requests.get(base_url, headers=headers)
-    html = BeautifulSoup(r.content, "lxml")
+from amazon_scraper import scrape_amazon
 
 
 if __name__ == '__main__':
     toode = input("Sisestage otsitav toode: ")
-    scrape_amazon(toode, "de")
+    amazon_scrapes_us = scrape_amazon(toode, "com")
+    amazon_scrapes_uk = scrape_amazon(toode, "co.uk")
+    amazon_scrapes_german = scrape_amazon(toode, "de")
+    with open("test.txt", "w", encoding="utf-8") as file:
+        for listing in amazon_scrapes_german:
+            file.write(f"{listing}\n")
+        for listing in amazon_scrapes_uk:
+            file.write(f"{listing}\n")
+        for listing in amazon_scrapes_us:
+            file.write(f"{listing}\n")
